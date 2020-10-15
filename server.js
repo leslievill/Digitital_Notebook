77169ce7dc6ef4e288d8a7d7a47205f1f3a974e5
Leslie Villatoro <levill98@gmail.com>
2020-10-15 14:44:51 -0700

    server working

---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const fs = require("fs");
+
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080
+
+// Sets up the Express app to handle data parsing
+// =============================================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/assets", express.static("./assets"));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+
+require("./routing/html-routes")(app);
+require("./routing/api-routes")(app); 
